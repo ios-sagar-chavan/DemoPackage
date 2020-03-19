1170cf6948e73001f4f3562cdd8db299eeaa215e
ios-sagar-chavan <61308390+ios-sagar-chavan@users.noreply.github.com>
2020-03-19 15:58:32 +0530

    Update DemoTest.swift

--- Sources/DemoPackage/DemoTest.swift
+++ Sources/DemoPackage/DemoTest.swift
@@ -9,10 +9,13 @@ import Foundation
 open class DemoTest{
     public static let `default` = DemoTest()
     
-    open func compareStrings(first: String, second: String)->Bool{
-        if(first==second)
+    open func compareStrings(first: String, second: String)->Bool{{
+        if(first==second){
             return true
+        }
         else
+        {
             return false
+        }
     }
 }
